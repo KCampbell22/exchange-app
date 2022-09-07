ade31da6225386f1c554716858bcb369fea369e1
KCampbell22 <ksoup12@gmail.com>
2022-09-07 08:42:16 -0500

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,6 +20,18 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <div className="col-12 d-flex my-3 justify-content-center">
+        <div className="col-6 d-flex my-3 align-items-center flex-column">
+          <h2>Exchange Rates</h2>
+          <h3>Type in your amount and see the exchange rates.</h3>
+
+          <div className="currency-box">
+            <Link className="currency-link" to="/rates">
+              Exchange Rates
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/rates.jsx
+++ src/components/rates.jsx
@@ -7,37 +7,42 @@ class Rates extends Component {
     return (
       <React.Fragment>
         <NavBar />
-        <div className="col-12 p">
-          <form>
-            <div className="form-group my-4 mx-2">
-              <label htmlFor="exchangeAmount">Amount</label>
-              <input type="text" name="exchangeAmount" id="exchange-amount" />
-            </div>
-          </form>
-        </div>
-        <div className="table-responsive col-6">
-          <table className="col-6 table">
-            <thead>
-              <tr>
-                <th scope="col">Currency</th>
-                <th scope="col">Rate</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr scope="row">
-                <td>USD</td>
-                <td>1.00</td>
-              </tr>
-              <tr scope="row">
-                <td>EUR</td>
-                <td>0.89</td>
-              </tr>
-              <tr scope="row">
-                <td>GBP</td>
-                <td>0.78</td>
-              </tr>
-            </tbody>
-          </table>
+        <div className="col-5 m-auto">
+          <div className="col-12 p">
+            <form>
+              <div className="form-group my-4 mx-2 d-flex flex-column">
+                <label className="me-2" htmlFor="exchangeAmount">
+                  Amount
+                </label>
+                <input type="text" name="exchangeAmount" id="exchange-amount" />
+                <p>1.00</p>
+              </div>
+            </form>
+          </div>
+          <div className="table-responsive col-12 m-auto">
+            <table className="col-6 table">
+              <thead>
+                <tr>
+                  <th scope="col">Currency</th>
+                  <th scope="col">Rate</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr scope="row">
+                  <td>USD</td>
+                  <td>1.00</td>
+                </tr>
+                <tr scope="row">
+                  <td>EUR</td>
+                  <td>0.89</td>
+                </tr>
+                <tr scope="row">
+                  <td>GBP</td>
+                  <td>0.78</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </React.Fragment>
     );
